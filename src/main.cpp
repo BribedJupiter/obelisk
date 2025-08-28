9e4a0fd595b4efd3f50ba265ca38c4791aca7de3
Jack <jgb1017@gmail.com>
2025-08-28 16:42:08 -0500

    Working triangle using shaders. Using GLAD for extension loading.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
+#include "glad/glad.h"
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
+
+#include <exception>
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <string>
+
+std::string loadShaders(const std::string filename) {
+    std::ifstream file;
+    file.exceptions(file.exceptions() | std::ios::failbit);
+    try {
+        file.open(filename.c_str());
+    } catch (std::exception e) {
+        std::cout << "Error opening " << filename  << " -> " << e.what() << std::endl;
+        return "";
+    }
+
+    std::string text((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    file.close();
+    return text;
+}
 
 int main() {
     // OpenGL Context Setup
@@ -20,8 +42,97 @@ int main() {
         return -1;
     };
 
+    // Initialize OpenGL extension loader
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(sf::Context::getFunction))) {
+        return -1;
+    }
+
+    // Load Shaders
+    std::string vertexShaderString = loadShaders("{PROJECT_PATH}/src/vertexShader.glsl");
+    std::string fragmentShaderString = loadShaders("{PROJECT_PATH}/src/fragmentShader.glsl");
+
     // GL Setup
     glClearColor(0.1f, 0.0f, 0.1f, 1.0f);
+
+    // See the LearnOpenGL textbook
+    // Constants for sandbox
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    // Create a vertex array object (VAO) to store vertex attribute states
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // Create a vertex buffer object to store the vertex data
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Compile vertex shader
+    const char* vertexShaderSource = vertexShaderString.c_str();
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Check if vertex shader compilation was successful
+    int vertexShaderCompilationSuccess;
+    char vertexInfoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexShaderCompilationSuccess);
+    if (!vertexShaderCompilationSuccess) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, vertexInfoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << vertexInfoLog << std::endl;
+    }
+
+    // Compile fragment shader
+    const char* fragmentShaderSource = fragmentShaderString.c_str();
+    unsigned int fragmentShader;
+    fragmentShader= glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Check if fragment shader compilation was successful
+    int fragmentShaderCompilationSuccess;
+    char fragmentInfoLog[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragmentShaderCompilationSuccess);
+    if (!fragmentShaderCompilationSuccess) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, fragmentInfoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << fragmentInfoLog << std::endl;
+    }
+
+    // Setup shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Check for shader program success
+    int shaderProgramSuccess;
+    char programInfoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &shaderProgramSuccess);
+    if (!shaderProgramSuccess) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, programInfoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << programInfoLog << std::endl;
+    }
+
+    // Delete the now unneeded (after linking) shader objects
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Link the vertex attributes
+    // Note that the previous VBO is still bound, so this will apply to that
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Prepare to draw
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
 
     bool running = true;
     while (running) {
@@ -39,8 +150,10 @@ int main() {
 
         // Clear buffers
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Draw
+        static int xPos = 0;
 
         // End the frame (internally swaps front and back buffers)
         window.display();
