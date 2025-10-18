e93f3d26f7f9c674e41a1ee4e7bce99f206b55bd
Jack <jgb1017@gmail.com>
2025-10-18 15:09:38 -0500

    Added GLM matrix math library dependency

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <SFML/OpenGL.hpp>
 #include <filesystem>
 #include <stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "obShader.h"
 
